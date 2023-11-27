991a35da4b7e88e8d529af4e38c17d4316d8875d
Planteig <Planteig17@hotmail.com>
2023-11-27 20:50:37 +0100

    d

--- main.js
+++ main.js
@@ -1,16 +1,119 @@
-let apiKey = prompt("api-key");
-const submitButton = document.querySelector("#generate-button")
-const inputField = document.querySelector("#input-drinks")
-const recipeField = document.querySelector(".recipe")
-const loaderAnimation = document.querySelector(".loader")
+// Contained Within Gather ApiKey Page
+let apiKey;
+const submitAPIKeyButton = document.querySelector("#input-api-button");
+const inputAPIKeyField = document.querySelector("#input-api-key");
 
+// Contained Within Gather Input For Drinks Page
+const submitButton = document.querySelector("#generate-button");
+const inputField = document.querySelector("#input-drinks");
+
+// Loading Animation
+const loaderAnimation = document.querySelector(".loader");
+
+// Pages
+const apiKeyPage = document.querySelector(".get-api-key");
+const drinksPage = document.querySelector(".get-drink");
+
+// Recipe guide
+const recipeField = document.querySelector(".recipe");
+let initialToggle = true
+
+// Ingredients list
+const ingredients = document.querySelectorAll(".ingredient")
+
+
+// Get ApiKey
+submitAPIKeyButton.addEventListener("click", async () => {
+    loadingAnimation(submitAPIKeyButton)
+    apiKey = inputAPIKeyField.value;
+    if (apiKey && await isValidApiKey(apiKey)) {
+        changeSite();
+        loadingAnimation(submitAPIKeyButton)
+    } else {
+        window.alert("Please insert a valid Api-key");
+        loadingAnimation(submitAPIKeyButton)
+    }
+});
+// Validate ApiKey
+async function isValidApiKey(apiKey) {
+    try {
+        const response = await fetch('https://api.openai.com/v1/chat/completions', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': `Bearer ${apiKey}`
+            },
+            body: JSON.stringify({
+                model: 'gpt-3.5-turbo',
+                messages: [{
+                    role: 'user',
+                    content: 'Is this a valid API key?'
+                }]
+            })
+        });
+        // Check if the response status is not okay
+        if (!response.ok) {
+            return false;
+        }
+        // Parse the response
+        const repsonseJSON = await response.json();
+
+        // Check if the response contains an error
+        if (repsonseJSON.error) {
+            return false;
+        }
+        // If everything is okay, return true
+        return true;
+    } catch (error) {
+        console.error('Error validating API key:', error);
+        return false;
+    }
+}
+
+// Toggle between sites
+function changeSite () {
+    apiKeyPage.classList.toggle("hidden")
+    drinksPage.classList.toggle("hidden")
+}
+
+// Check if user selected some predefined ingredients
+
+ingredients.forEach((ingredient) => {
+    ingredient.addEventListener("click",() => {
+        ingredient.classList.toggle("selected")
+    })
+})
+// Add to the fetch
+let listOfIngredients = [];
+function ingredientsData () {
+    //Clear the array
+    listOfIngredients = []
+    //Find the selected ingredients
+    ingredients.forEach((ingredient) => {
+        if (ingredient.classList.contains("selected")) {
+            let currentIngredient = ingredient.querySelector("h2").innerText;
+            listOfIngredients.push(currentIngredient)
+        }
+    })
+}
+
+// Generate Drink
 submitButton.addEventListener("click",() => {
+    ingredientsData()
+    listOfIngredients = listOfIngredients.join(", ")
+    console.log(listOfIngredients)
+
+    if (initialToggle === false) {
+        showRecipeField()
+    }
+
     // Animation and clear if there is already an item.
     recipeField.innerHTML = "";
     inputField.innerHTML = "";
-    loading()
+    loadingAnimation(submitButton);
 
     let listOfIngredients = inputField.value
+
 
     fetch('https://api.openai.com/v1/chat/completions', {
         method: 'POST',
@@ -22,22 +125,33 @@ submitButton.addEventListener("click",() => {
             model: 'gpt-3.5-turbo',
             messages: [{
                 role: 'user',
-                content: `Please respond with a drink made out of ${listOfIngredients}. Also give it a funny and matching name. Please provide a detailed list of ingredients and a guide `
+                content: `Please respond with a drink made out of ${listOfIngredients}. Also give it a funny and matching name. Please provide a detailed list of ingredients and a guide`
             }]
         })
     })
         .then(response => response.json())
         .then((data) => {
-            loading()
+            loadingAnimation(submitButton)
+
             let pElement = document.createElement("p")
             pElement.innerText = data.choices[0].message.content
             recipeField.append(pElement)
-            console.log(data.choices[0].message.content)
-        })
+
+            //
+            initialToggle = false
+            showRecipeField()
+
+        }).catch((error) => {
+        console.log("There was an error trying to receive your drink. Please try again later.", error)
+    })
 })
 
-function loading () {
+function loadingAnimation (button) {
     loaderAnimation.classList.toggle("hidden")
-    submitButton.classList.toggle("hidden")
+    button.classList.toggle("hidden")
+}
+
+function showRecipeField () {
+    recipeField.classList.toggle("hidden")
 }
 
